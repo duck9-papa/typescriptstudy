af059cbf9d59fa5ed8f3336b8f4670341061ab76
duck9-papa <107654769+duck9-papa@users.noreply.github.com>
2022-11-08 23:35:03 +0900

    type

---
+++ src/study/Type.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Type(): any {
+  const user1 = 'inho';
+
+  let user2 = 'inho';
+
+  interface Dog {
+    color: 'brown';
+    weight: number;
+    bow(): void;
+  }
+  interface Human {
+    color: 'brown';
+    weight: number;
+    wait(): void;
+  }
+
+  return <></>;
+}
+
+export default Type;
